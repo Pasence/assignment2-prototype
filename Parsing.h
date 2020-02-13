c7ea9aad9ab013d54bd3a7e3021bfc52d167be0d
Pasence <59620641+Pasence@users.noreply.github.com>
2020-02-13 15:06:50 -0800

    create parsing.h

---
+++ Parsing.h
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef PARSING_H
+#define PARSING_H
+
+#include <vector>
+#include <string>
+
+
+using namespace std;
+class Parsing
+{
+public : 
+	void Parsetoken(vector <string>& CmdTkns,
+		vector <string>& ArgTkns,
+		vector <string>& SepTkns,
+		//vector <string>& qouted,
+		string& mystring);
+
+};
+#endif // !PARSING_h
